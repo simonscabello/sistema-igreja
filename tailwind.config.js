0e4be84d64e9bfc8c3dcfe81bdb6fe51f77bef3b
Simon Scabello <simon.scabello@live.com>
2025-06-18 23:54:10 -0300

    Atualiza a configuração do Tailwind para alterar a fonte padrão de 'Figtree' para 'Fira Sans', melhorando a estética tipográfica do aplicativo.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Fira Sans', ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 primary: {
