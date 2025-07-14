7a31bb82a63cefb3ec5fb4ca260b52ef88834120
Jennifer <duarte.neni@gmail.com>
2025-07-13 23:13:23 -0300

    Atualiza a imagem de fundo da página de login e ajusta o layout para aumentar a largura do container, melhorando a apresentação visual e a responsividade da interface.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -31,7 +31,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover bg-center bg-no-repeat relative" style="background-image: url('/bg-login.jpg')">
             <div class="absolute inset-0 bg-black bg-opacity-20"></div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg transition-colors duration-300 relative z-10">
+            <div class="w-full sm:max-w-lg mt-1 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg transition-colors duration-300 relative z-10">
                 {{ $slot }}
             </div>
         </div>
