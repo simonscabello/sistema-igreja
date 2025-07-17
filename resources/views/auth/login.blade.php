c77429d488bb00dba7be54284f2a6579ffcdbbfd
Simon Scabello <simon.scabello@live.com>
2025-07-17 15:41:29 -0300

    Atualiza a view de login para melhorar a exibição de mensagens de erro, garantindo que apenas erros relevantes sejam mostrados para os campos de e-mail e senha. Essa alteração aprimora a experiência do usuário ao fornecer feedback mais claro durante o processo de autenticação.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,19 +14,16 @@
         <div>
             <x-input-label for="email" :value="__('E-mail')" required="true" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-error :messages="$errors->get('email') && !collect($errors->get('email'))->contains(trans('auth.failed')) ? $errors->get('email') : []" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Senha')" required="true" />
-
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Remember Me -->
